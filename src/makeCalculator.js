75f48d254c06e07ca7ad248fafcd4ca86233d41d
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-04-17 23:37:19 +0300

    some message

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -8,7 +8,9 @@ function makeCalculator() {
     result: 0,
 
     operate(callback, number) {
-      callback(number);
+      if (typeof callback === 'function') {
+        callback(number);
+      }
 
       return calculator;
     },
